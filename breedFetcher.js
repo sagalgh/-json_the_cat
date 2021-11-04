14f98bb54cf4b4b52c6e52bd09144971ee5cc425
Sagal Ghelle <sagal.ghelle@gmail.com>
2021-11-04 19:48:01 -0400

    updated

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,16 +4,17 @@ const request = require('request');
 
 
 const fetchBreedDescription = function(breedName, callback) {
-  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   request(url, (error, response, body) => {
     const data = JSON.parse(body);
-    const description = data.length > 0? data[0].description : null
-    callback(error,description );
+    const description = data.length > 0 ? data[0].description : null;
+    callback(error,description);
     
-})}
+  });
+};
  
 
 
 
 
-module.exports = {fetchBreedDescription}
+module.exports = {fetchBreedDescription};
