c056cdf0c7d305a1a88996556781e4019a079fc5
Sagal Ghelle <sagal.ghelle@gmail.com>
2021-11-04 19:47:07 -0400

    updated

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,19 @@
 const request = require('request');
 //https://api.thecatapi.com/v1/breeds/search
-const args = process.argv.slice(2);
-const breedName = args[0];
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  const data = JSON.parse(body);
-  //console.log(typeof data);
-  //console.log(data);
-  if (data.length){
-  const description = data[0].description;
-  console.log(description);
-  //console.log('body:', body); // Print the HTML for the given page
-  }
-  if (!data.length) { 
-    console.error(`Failed: ${breedName} does not exist`);
-    return;
-  }
-})
+
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`
+  request(url, (error, response, body) => {
+    const data = JSON.parse(body);
+    const description = data.length > 0? data[0].description : null
+    callback(error,description );
+    
+})}
+ 
 
 
 
 
+module.exports = {fetchBreedDescription}
---
+++ index.js
@@ -0,0 +1,13 @@
+//This file will be the one that users can run and provide the breed name to.
+
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
